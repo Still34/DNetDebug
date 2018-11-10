13ac2cf0fc177d5aacdc2663b5d9cc2b7964130b
Casino Boyale <kayrenhi@gmail.com>
2018-11-10 14:13:24 +0000

    Bug fix

--- DNetDebug/Program.cs
+++ DNetDebug/Program.cs
@@ -21,6 +21,8 @@ namespace DNetDebug
 
         public async Task StartAsync()
         {
+            Console.WriteLine("Casino was here");
+            
             var services = ConfigureService();
             var restClient = services.GetRequiredService<DiscordRestClient>();
             var client = services.GetRequiredService<DiscordSocketClient>();
